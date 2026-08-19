ae0eda15f38844c513c38a285224b01e369308fe
Bohdan <ipatov.bohdan@gmail.com>
2026-08-19 21:02:03 +0300

    deleted product component file and added retry decorator

--- components/product.py
+++
@@ -1,9 +0,0 @@
-"""Product entity: data and action components."""
-
-from clients.api import Api
-
-
-class ProductEntity:
-    def __init__(self, api: Api) -> None:
-        self.facade = api
-        # self.ui = ProductUiActions(self)
---
+++ utils/decorators.py
@@ -0,0 +1,31 @@
+import functools
+import logging
+import time
+
+logger = logging.getLogger("autotest.decorators")
+
+
+def retry(times=3, pause=1, exceptions=(Exception,)):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            last_exception = None
+            for attempt in range(1, times + 1):
+                try:
+                    return func(*args, **kwargs)
+                except exceptions as e:
+                    last_exception = e
+                    if attempt < times:
+                        logger.info(
+                            "%s failed on attempt %d/%d (%s: %s), retrying in %.1fs",
+                            func.__name__, attempt, times, type(e).__name__, e, pause,
+                        )
+                        time.sleep(pause)
+                    else:
+                        logger.warning(
+                            "%s failed on attempt %d/%d (%s: %s), giving up",
+                            func.__name__, attempt, times, type(e).__name__, e,
+                        )
+            raise last_exception
+        return wrapper
+    return decorator
